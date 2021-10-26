8a96a89fac13f627e9901299d1de2e76b6b0491e
Nadiia Yatsiuk <yatsiukni@gmail.com>
2021-10-25 22:55:00 -0400

    Homework

---
+++ src/SecondHM.java
@@ -0,0 +1,8 @@
+
+
+public class SecondHM {
+    public static void main(String[] args) {
+        int building=158;
+        System.out.println("Building hights is " + Math.max(158, 5897));
+    }
+}
