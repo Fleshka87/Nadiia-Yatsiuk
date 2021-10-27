9251ef02ff48865c4abc2cf6cb6476c078d757c6
Nadiia Yatsiuk <yatsiukni@gmail.com>
2021-10-27 16:20:32 -0400

    added new java class

--- src/SecondHM.java
+++
@@ -1,8 +0,0 @@
-
-
-public class SecondHM {
-    public static void main(String[] args) {
-        int building=158;
-        System.out.println("Building hights is " + Math.max(158, 5897));
-    }
-}
---
+++ src/homeWork.java
@@ -0,0 +1,7 @@
+public class homeWork {
+    public static void main(String[] args) {
+        int buildingHigts = 250;
+        int secongBuildingHigts = 255;
+        System.out.println("Building hights is " + Math.max(250, 255));
+    }
+}
