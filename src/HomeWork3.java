b3ae6255eb7b7033cfa0564628b6fe941dd20c35
Nadiia Yatsiuk <yatsiukni@gmail.com>
2021-11-01 20:24:19 -0400

    cycle added to the file

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class HomeWork3 {
     public static void main(String[] args) {
         System.out.println("If I drink, I die. If I eat, I am fine. What am I?");
-        Scanner scan = new Scanner(System.in);
-        String answer= scan.nextLine();
-        switch (answer){
-            case"Fire":
-                System.out.println("Great");
-                break;
-            case "I don’t know":
-                System.out.println("Answer: Fire");
-                break;
-            default:
-                System.out.println("Think again");
-        }
 
+        for (int i = 0; i < 3; i++) {
+            Scanner scan = new Scanner(System.in);
+            String answer = scan.nextLine();
+
+            switch (answer) {
+                case "Fire":
+                    System.out.println("Great");
+                    break;
+                case "I don’t know":
+                    System.out.println("Answer: Fire");
+                    break;
+                default:
+                    System.out.println("Think again");
+            }
+        }
     }
-}
+    }
+
+
